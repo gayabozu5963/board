d6aa2be63640cb0680836c727079ca3b6be6efdf
takaokouki <cacaoporky111246@gmail.com>
2020-11-01 00:19:06 +0900

    uuid delete
    uuid いらなかった

--- database/migrations/2020_10_31_120221_change_user_table.php
+++ database/migrations/2020_10_31_120221_change_user_table.php
@@ -15,7 +15,7 @@ class ChangeUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid('uuid');
+            // $table->uuid('uuid');
         });
     }
 
